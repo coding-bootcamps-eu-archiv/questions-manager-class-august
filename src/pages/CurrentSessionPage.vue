0f3218ab8c44dc6f4dedaaadcc40e726f41c3d5d
KadirBalku <kadir.balku@gmail.com>
2022-11-10 21:04:37 +0100

    feat: implement route to single session and rendering questions

--- src/pages/CurrentSessionPage.vue
+++ src/pages/CurrentSessionPage.vue
@@ -1,13 +1,14 @@
 <template>
-  <section class="sessions" id="sessions">
-    <ul class="sessions__list" id="sessions-list">
+  <section class="questions" id="questions">
+    <ul class="questions__list" id="questions-list">
       <li
-        class="sessions__element"
+        class="questions_element"
         v-for="question in questions"
         :key="question.id"
       >
-        <div class="session__wrapper">
-          <h3 class="session__name">{{ question }}</h3>
+        <div class="question__wrapper">
+          <p>{{ dayJS(question.createdAt).fromNow() }}</p>
+          <h3 class="question__name">{{ question.question }}</h3>
           <!-- <p class="session__date">
             {{ dayJS(session.createdAt).fromNow() }}
           </p> -->
@@ -16,7 +17,7 @@
     </ul>
 
     <!-- will be removed after handling all related issues -->
-    <pre>{{ sessionId }}</pre>
+    <pre>{{ questions }}</pre>
   </section>
 </template>
 
@@ -24,21 +25,17 @@
 export default {
   inject: ["dayJS"],
   data() {
-    return { sessionId: this.$route.query.sessionId, questions: [] };
+    return { questions: [] };
   },
-  created() {
-    const evtSource = new EventSource(
-      process.env.VUE_APP_API_BASE_URL + "/stream/" + this.sessionId
+  async created() {
+    //will be replaced with streaming function of api in a later issue (see api docs)
+    const response = await fetch(
+      process.env.VUE_APP_API_BASE_URL +
+        "/questions?sessionId=" +
+        this.$route.params.id
     );
 
-    evtSource.addEventListener(
-      "message",
-      (event) => {
-        console.log(JSON.parse(event.data));
-        this.questions = JSON.parse(event.data);
-      },
-      false
-    );
+    this.questions = await response.json();
   },
 };
 </script>
@@ -49,56 +46,46 @@ li {
   all: unset;
 }
 
-.sessions {
+.questions {
   margin: auto;
   max-width: 100ch;
 }
-.sessions__list {
+.questions__list {
   display: flex;
   flex-direction: column;
   padding: 0.5rem;
 }
 
-.sessions__element {
+.questions_element {
   display: flex;
   align-items: center;
   width: 100%;
 }
-.session__wrapper {
+.question__wrapper {
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  align-items: flex-start;
   padding: 0.5rem;
   border: 1px solid var(--clr-primary);
   cursor: pointer;
-  flex-grow: 100;
+  gap: 2rem;
+  width: 100%;
 }
-.session__name {
+.question__name {
   font-size: 1rem;
   font-weight: normal;
   color: var(--clr-text);
 }
-.session__date {
+.question__date {
   font-size: 0.75rem;
   font-weight: normal;
   color: var(--clr-primary-inactive);
 }
-.session__wrapper:hover .session__name {
+.question__wrapper:hover .question__name {
   color: var(--clr-primary);
   text-decoration: underline;
 }
 
-.btn__wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  padding: 0.5rem;
-  gap: 0.5rem;
-  flex-grow: 1;
-}
-.btn {
-  all: unset;
-}
 .open {
   font-size: 1.5rem;
   color: var(--clr-primary-inactive);
