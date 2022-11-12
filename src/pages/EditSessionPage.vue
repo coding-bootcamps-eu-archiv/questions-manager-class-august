775783ce57f567fb13afbc934ece386a47b0f611
claudia.sollinger@gmail.com <claudia.sollinger@gmail.com>
2022-11-12 12:30:47 +0100

    FEAT: change question status

--- src/pages/EditSessionPage.vue
+++ src/pages/EditSessionPage.vue
@@ -25,7 +25,7 @@
               @change="onCheckChangeState"
               class="open"
               :id="question.id"
-              :checked="question.done"
+              :checked="!question.open"
             />
             <label :for="question.id" class="open__label"
               ><svg
@@ -90,9 +90,10 @@
           <div class="btn__wrapper">
             <input
               type="checkbox"
+              @change="onCheckChangeState"
               class="open"
               :id="question.id"
-              :checked="question.open"
+              :checked="!question.open"
             />
             <label :for="question.id" class="open__label"
               ><svg
