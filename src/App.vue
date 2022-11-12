0146b7ca66bd954d3a7d4695f0962989305a5bed
NonoSikk <nina.maerte@hotmail.de>
2022-11-12 13:08:12 +0100

    FEAT: add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <LayoutHeader />
   <router-view />
+  <LayoutFooter />
 </template>
 
 <script>
 import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+
 export default {
   components: {
     LayoutHeader,
+    LayoutFooter,
   },
 };
 </script>
