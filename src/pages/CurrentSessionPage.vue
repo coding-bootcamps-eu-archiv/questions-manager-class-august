2a3f1f57416477c7e7cac2079f2d0f118f7d0eea
NonoSikk <nina.maerte@hotmail.de>
2022-11-14 18:37:15 +0100

    FIX: Stduent view

--- src/pages/CurrentSessionPage.vue
+++ src/pages/CurrentSessionPage.vue
@@ -223,11 +223,13 @@ li {
 }
 .header__open {
   color: rgb(94, 189, 94);
-  padding-bottom: 0.5rem;
+  font-size: 1.5rem;
+  margin-bottom: -10px;
 }
 .header__answered {
   color: salmon;
-  padding-bottom: 0.5rem;
+  font-size: 1.5rem;
+  margin-bottom: -10px;
 }
 .questions {
   display: flex;
